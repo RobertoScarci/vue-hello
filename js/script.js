aa2704bbf2de04cd129187eb32281e789c93e895
RobertoScarci <robertoscarci.02@gmail.com>
2023-11-30 19:02:09 +0100

    inserimento dell'immagine tramite vue

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            title: 'Ciao Mondo'
+            title: 'You were my BROTHER ANAKIN!',
+            source: "https://w0.peakpx.com/wallpaper/972/188/HD-wallpaper-anakin-vs-obiwan-darth-episode-obiwan-star-wars-obi-wan-vader-3-anakin.jpg"
         }
     },
 }).mount('#app');
+
